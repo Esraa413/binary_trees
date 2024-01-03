50e9d5474d6c0b3cbc0b27bda329156bd3462e01
esraa413 <esraaabdelwahab614>
2024-01-04 01:32:33 +0200

    trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,7 @@
  * binary_tree_height -  a function that measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  *
- * Return: 
+ * Return: Always 0 
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
