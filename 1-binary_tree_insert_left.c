2cef897206d34df87e28177ebb4b35b0a25cb6bd
Esraa413 <esraaabdelwahab614@gmail.com>
2024-01-03 13:41:40 -0800

    trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,9 +13,8 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = NULL;
 
-	node = NULL;
 	if (parent == NULL)
 		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
