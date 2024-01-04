0323bfa23a236f8a982d63980f3f747986bcf279
esraa413 <esraaabdelwahab614>
2024-01-04 02:26:57 +0200

    trees

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,7 +3,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - a function that goes through a binary tree using in-order traversal
+ * binary_tree_inorder - a function that goes through,
+ * binary tree using in-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  */
