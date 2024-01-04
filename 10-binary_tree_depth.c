95a2cb57cf23bd400fe851e44737f4bd3f1732ea
esraa413 <esraaabdelwahab614>
2024-01-04 14:35:30 +0200

    trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - a function that measures depth of a node in a binary tree
+ * binary_tree_depth - a function that measures depth of a node in binary tree
  * @tree: pointer to the node to measure the depth
  *
  * Return: Always 0
