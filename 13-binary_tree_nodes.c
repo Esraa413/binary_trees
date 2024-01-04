2041d40d9e2344f8ff29243c0c1a2dc709f8228f
esraa413 <esraaabdelwahab614>
2024-01-04 18:48:11 +0200

    trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_nodes - a function that counts the nodes with
  * at least 1 child in a binary tree
- * @@tree: pointer to the root node of the tree to count the number
+ * @tree: pointer to the root node of the tree to count the number
  *
  * Return: Always 0
  */
